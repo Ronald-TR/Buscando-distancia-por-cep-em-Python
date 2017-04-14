deaaac9182bc28d25ceacdef188ec770522bade4
Ronald <ronald-farias@outlook.com>
2017-04-14 01:44:35 -0300

    Create Rota.py

---
+++ Rota.py
@@ -0,0 +1,17 @@
+def rota(cep1, cep2):
+    url1 = 'https://viacep.com.br/ws/' + cep1 + '/json/'
+    url2 = 'https://viacep.com.br/ws/' + cep2 + '/json/'
+    origem = json.loads(requests.get(url1).text)
+    destino = json.loads(requests.get(url2).text)
+    strOrigem = origem['localidade'] + ' ' + origem['uf']
+    strOrigem = strOrigem.replace(' ', '+')
+    strDestino = destino['localidade'] + ' ' + destino['uf']
+    strDestino = strDestino.replace(' ', '+')
+    return requests.get('https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins='
+                     +strOrigem+'&destinations='+strDestino)    #adicione o campo &key= ao final, com a sua chave de api distancematrix,
+                                                                #obtida gratuitamente no seu site oficial
+                                                                
+                                                                
+#Exemplo de chamada
+resultado = rota('65066210', '65320000')
+print(resultado.text)
